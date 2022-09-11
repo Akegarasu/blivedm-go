24c237f25b4be6d34e9821d346b44f3877d46272
Akiba <akegarasu.akiba@gmail.com>
2022-09-11 15:32:43 +0800

    :zap: add more examples

---
+++ example/dump/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Akegarasu/blivedm-go/client"
+	_ "github.com/Akegarasu/blivedm-go/utils"
+	log "github.com/sirupsen/logrus"
+	"github.com/tidwall/gjson"
+)
+
+const roomId = "8792912"
+
+var dumps = []string{"GUARD_BUY", "USER_TOAST_MSG"}
+
+func main() {
+	log.SetLevel(log.DebugLevel)
+	c := client.NewClient(roomId)
+	for _, v := range dumps {
+		vv := v
+		c.RegisterCustomEventHandler(vv, func(s string) {
+			data := gjson.Get(s, "data").String()
+			fmt.Printf("[%s] %s\n", vv, data)
+		})
+	}
+	err := c.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("started bili dumper")
+	select {}
+}
