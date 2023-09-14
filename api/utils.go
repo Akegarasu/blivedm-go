6833089dc90180a05ed1626ad313dea8f3ecc682
akiba <akiba@anzu.link>
2023-09-14 20:44:46 +0800

    update

--- api/utils.go
+++ api/utils.go
@@ -2,8 +2,27 @@ package api
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 )
+
+func HttpGet(url string, headers *http.Header) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header = *headers
+	c := &http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
 
 func GetJson(url string, result interface{}) error {
 	resp, err := http.Get(url)
